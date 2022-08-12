3f30ca7b2faee50d5a89d0982ab49e16e8d3f9ef
unknown <othman.alkhatib.en@gmail.com>
2022-08-12 19:16:51 +0300

    files

--- meden.py
+++ meden.py
@@ -1,14 +1,16 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 import time
+import os
 
 class Melden:
     def __init__(self):
         self.op = webdriver.ChromeOptions()
-        self.op.add_argument('headless')
-        self.driver = webdriver.Chrome(executable_path="C:\chromedriver.exe", options=self.op)
-
-        self.driver.get("https://otv.verwalt-berlin.de/ams/TerminBuchen/wizardng/ad889662-ea23-4531-ace3-1f1564c62f71;jsessionid=RniDnAVxpXktXVxtF_mceXWtDg0zFg0wPAiqzEM6.frontend-2?dswid=2275&dsrid=668&st=2&v=1659631472533")
+        self.op.binary_location = os.getenv("GOOGLE_CHROME_BIN")
+        self.op.add_argument('--headless')
+        self.op.add_argument("--disable--dev--shm--usage")
+        self.op.add_argument("--no-sandbox")
+        self.driver = webdriver.Chrome(executable_path=os.getenv("CHROMEDRIVER_PATH"), options=self.op)
 
     def searchSubmitButton(self):
         global button
@@ -20,6 +22,7 @@ class Melden:
             return False
 
     def checkSite(self):
+        self.driver.get("https://otv.verwalt-berlin.de/ams/TerminBuchen/wizardng/ad889662-ea23-4531-ace3-1f1564c62f71;jsessionid=RniDnAVxpXktXVxtF_mceXWtDg0zFg0wPAiqzEM6.frontend-2?dswid=2275&dsrid=668&st=2&v=1659631472533")
         while not self.searchSubmitButton():
             time.sleep(1)
 
