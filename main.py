b0060e33d5b76b0452e1f9a1e05f1a55795a56dc
unknown <othman.alkhatib.en@gmail.com>
2022-08-15 19:26:38 +0300

    new edit

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from telegram.ext import CommandHandler, Updater, CallbackContext
+from telegram.ext import CommandHandler, Updater, CallbackContext, MessageHandler, Filters
 from telegram import Update
 import os
 import logging
@@ -29,37 +29,107 @@ else:
     sys.exit(1)
 
 
+ml = Melden()
+updater = Updater(TOKEN)
+
+
 def send_message(update:Update, context:CallbackContext):
     update.message.reply_text(update.message.chat.id)
 
+is_site_open = 0
+is_site_closed = 0
+def check_site_reply():
+    global is_site_open, is_site_closed, updater, ml
+    if not ml.checkSite("https://www.ecsc.gov.sy/"):
+        print("closed")
+        if not is_site_closed:
+            updater.bot.sendMessage(chat_id='872679970', text='Site is Closed')
+            print("closed")
+            is_site_closed = 1
+            is_site_open = 0
+        return False
+    else:
+        print("open")
+        if not is_site_open:
+            updater.bot.sendMessage(chat_id='872679970', text='Site is Open')
+            is_site_open = 1
+            is_site_closed = 0
+        return True
+
+
+is_doc_open = 0
+is_doc_closed = 0
+def check_doc_reply():
+    global is_doc_open, is_doc_closed, updater, ml
+    if not ml.checkDocument():
+        print("closed")
+        if not is_doc_closed:
+            updater.bot.sendMessage(chat_id='872679970', text='Unfortuinatly, Making Document is Closed Again')
+            is_doc_closed = 1
+            is_doc_open = 0
+    else:
+        print("open")
+        if not is_doc_open:
+            updater.bot.sendMessage(chat_id='872679970', text='Quickly! You Can Make Document')
+            is_doc_open = 1
+            is_doc_closed = 0
+
+
+PERSONAL_NUMBER = os.getenv("PERSONAL_NUMBER")
+PASSWORD = os.getenv("PASSWORD")
 def check_ecsc():
-    updater = Updater(TOKEN)
-    ml = Melden()
-    for i in range(3):
-        if ml.checkSite():
-            updater.bot.sendMessage(chat_id='872679970', text='Hello there!, Go sign up')
-        else:
-            updater.bot.sendMessage(chat_id='872679970', text='Sorry, but there is no place')
+    global updater, ml
+    try:
+        if check_site_reply():
+            check_doc_reply()
+        time.sleep(120)
 
-    # op = webdriver.ChromeOptions()
-    # op.add_argument("headless")
-    # while True:
-    #     driver = webdriver.Chrome(executable_path="./chromedriver.exe", options=op)
-    #     driver.get("https://otv.verwalt-berlin.de/ams/TerminBuchen/wizardng/ad889662-ea23-4531-ace3-1f1564c62f71;jsessionid=RniDnAVxpXktXVxtF_mceXWtDg0zFg0wPAiqzEM6.frontend-2?dswid=2275&dsrid=668&st=2&v=1659631472533")
-    #     updater = Updater(TOKEN)
-    #     updater.bot.sendMessage(chat_id='872679970', text='Hello there!')
+    except Exception as error:
+        print(error)
+        updater.bot.sendMessage(chat_id='872679970', text=error)
+
+    time.sleep(5)
 
 
+def make_document(update:Update, context:CallbackContext):
+    try:
+        text = update.message.text.split(" ")
+        personal_number = text[1]
+        password = text[2]
+    except Exception as error:
+        updater.bot.sendMessage(chat_id='872679970', text='Incorrect Input')
 
-def start_check_ecsc_thread():
-    thread = threading.Thread(target=check_ecsc, args=())
-    thread.daemon = True
-    thread.start()
+    try:
+        ml2 = Melden()
+        ml2.checkSite("https://www.ecsc.gov.sy/")
+        ml2.login(personal_number, password)
+        ml2.checkDocument()
+        ml2.makeDocument()
+        updater.bot.sendMessage(chat_id='872679970', text='Document has been created')
+
+    except Exception as error:
+        print("=== ml2 error ===")
+        print(error)
+        updater.bot.sendMessage(chat_id='872679970', text='Sorry, Can not make document')
 
 
+# def start_check_ecsc_thread():
+#     thread = threading.Thread(target=check_ecsc, args=())
+#     thread.daemon = True
+#     thread.start()
 
 if __name__ == "__main__":
     updater = Updater(TOKEN, use_context=True)
     updater.dispatcher.add_handler(CommandHandler("start", send_message))
+    updater.dispatcher.add_handler(CommandHandler("make", make_document))
     run()
-    start_check_ecsc_thread()
+    while True:
+        if check_site_reply():
+            ml.login(PERSONAL_NUMBER, PASSWORD)
+            break
+    while True:
+        try:
+            check_ecsc()
+        except Exception as error:
+            print(error)
+    # start_check_ecsc_thread()
--- meden.py
+++ meden.py
@@ -1,7 +1,13 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 import time
+import pandas as pd
 import os
+
+login_url = "https://www.ecsc.gov.sy/login"
+personal_id = "mat-input-0"
+password_id = "mat-input-1"
+next_button_class = "mat-primary"
 
 class Melden:
     def __init__(self):
@@ -10,30 +16,87 @@ class Melden:
         self.op.add_argument('--headless')
         self.op.add_argument("--disable--dev--shm--usage")
         self.op.add_argument("--no-sandbox")
+        # os.getenv("CHROMEDRIVER_PATH")
         self.driver = webdriver.Chrome(executable_path=os.getenv("CHROMEDRIVER_PATH"), options=self.op)
 
-    def searchSubmitButton(self):
-        global button
+    def searchElement(self, selector):
+        global element
         try:
-            button = self.driver.find_element(By.ID, "applicationForm:managedForm:proceed")
+            element = self.driver.find_element(By.CSS_SELECTOR, selector)
             return True
         except Exception as error:
-            # print(error)
             return False
 
-    def checkSite(self):
-        self.driver.get("https://otv.verwalt-berlin.de/ams/TerminBuchen/wizardng/ad889662-ea23-4531-ace3-1f1564c62f71;jsessionid=RniDnAVxpXktXVxtF_mceXWtDg0zFg0wPAiqzEM6.frontend-2?dswid=2275&dsrid=668&st=2&v=1659631472533")
-        while not self.searchSubmitButton():
-            time.sleep(1)
+    def check(self):
+        for i in range(20) :
+            if self.searchElement("div[role='alertdialog']"):
+                return False
+            time.sleep(0.5)
+            print(i)
+        return True
 
-        button.click()
-        time.sleep(30)
+    def checkSite(self, url):
         try:
-            message = self.driver.find_element(By.CLASS_NAME, "errorMessage")
-            print("====== No Places ======")
-            return False
-            # time.sleep(60)
+            self.driver.get(url)
         except Exception as error:
-            # messagebox.showinfo("VISA", "Go Take Visa Now!")
-            print(error)
-            return True
+            return False
+        return self.check()
+
+    def const_select_options(self, select_index, option_index):
+        self.driver.find_elements(By.TAG_NAME, "mat-select")[select_index].click()
+        time.sleep(1)
+        self.driver.find_elements(By.TAG_NAME, "mat-option")[option_index].click()
+        time.sleep(1)
+
+    def click_button(self, index):
+        self.driver.find_elements(By.TAG_NAME, "button")[index].click()
+        time.sleep(5)
+
+    def login(self, personal_number, password):
+        self.driver.get(login_url)
+        personal_field = self.driver.find_element(By.ID, personal_id)
+        password_field = self.driver.find_element(By.ID, password_id)
+        next_button = self.driver.find_element(By.TAG_NAME, 'button')
+
+        personal_field.send_keys(personal_number)
+        password_field.send_keys(password)
+        next_button.click()
+        time.sleep(5)
+
+    def checkDocument(self):
+
+        # Step 2
+        # self.driver.get("https://www.ecsc.gov.sy/requests")
+        # self.driver.find_elements(By.TAG_NAME, "button")[3].click()
+        self.driver.get("https://www.ecsc.gov.sy/requests/process/new/new")
+        time.sleep(15)
+
+
+        # Step 3
+        # self.const_select_options(0, 5)
+        self.const_select_options(1, 1)
+        self.click_button(1)
+
+
+        # Step 4
+        self.const_select_options(2, 7)
+        self.const_select_options(3, 1)
+        self.const_select_options(4, 1)
+        self.driver.find_elements(By.CSS_SELECTOR, "input[type= 'number']")[1].send_keys(0)
+        self.const_select_options(5, 0)
+        self.const_select_options(6, 2)
+        self.click_button(3)
+
+        return self.check()
+
+    def makeDocument(self):
+        # Step 5
+        self.click_button(6)
+        time.sleep(5)
+
+        # Step 6
+        self.click_button(1)
+        self.click_button(4)
+        time.sleep(10)
+
+        self.driver.close()
